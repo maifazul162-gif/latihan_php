9c9040b822c64b6750c98279526e4cb3da0c7759
maifazul162-gif <maifazul162@gmail.com>
2025-09-23 15:03:03 +0700

    LATIHAN_PHP

---
+++ latihan1.php
@@ -0,0 +1,9 @@
+<?php
+$IPK=3.9;
+$string1 = <<<end
+Saya sedang belajar PHP
+di Politeknik Negeri Jember <br /> Kali ini tentang pembahasan
+mengenai "PHP", <br /> dan berharap bisa dapat IPK $IPK :)
+end;
+echo $string1;
+?>
